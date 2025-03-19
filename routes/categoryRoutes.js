ddde3354b9ee081af5b9ba561c8d38ac399739e7
sanajana <bsanjanamadival@gmail.com>
2025-03-19 18:00:30 +0530

    Updated categoryController and categoryRoutes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/categoryController');
+router.get("/exists", categoryController.checkCategoryExists);
 
 router.post('/add', categoryController.addCategory);
 router.get('/all', categoryController.getCategories);
