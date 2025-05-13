ddf637fb46dbac9e8d6aeb72dd4f33b5068f8a31
Suchitha <suchithabolar.ganglia@gmail.com>
2025-05-13 17:29:02 +0530

    Backend support page changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const logentryRoutes = require('./routes/logentryRoutes'); 
 const categoryRoutes = require('./routes/categoryRoutes');
 const uploadRoute = require('./routes/uploadRoute'); 
+const supportRoutes = require('./routes/supportRoutes');
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
@@ -54,7 +55,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/logentry', logentryRoutes);
 app.use('/api/category', categoryRoutes);
-
+app.use('/api', supportRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
